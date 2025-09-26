37ef70ae4eb56f61b10d8e1629d6ba666063586e
Tripurari Jha <tjha846@gmail.com>
2025-09-26 21:57:52 +0530

    07reactrouter

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p class="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white  font-semibold rounded-lg shadow-md">
+      Pretty background text ✨
+      </p>
+
+      
+    </>
+  )
+}
+
+export default App
